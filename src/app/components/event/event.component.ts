d9baacc6fa99b219134ab1cd1e971dc1ec8ab4a2
Subhdeep Saha <subhdeepsaha@gmail.com>
2017-03-24 18:19:55 +0530

    Add caching on Events

--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -6,6 +6,7 @@ import { MdDialog } from '@angular/material';
 import { CalendarComponent } from 'angular2-fullcalendar/src/calendar/calendar';
 
 import { EventDialogComponent } from './event-dialog/event-dialog.component';
+import { HttpWrapperService } from '../../services/http-wrapper.service';
 
 @Component({
   selector: 'app-event',
@@ -34,19 +35,24 @@ export class EventComponent implements OnInit {
         $('angular2-fullcalendar').fullCalendar('refetchEvents');
       });
     },
-    eventSources: [
-      {
-        url: 'https://dashboard.pclub.in/api/timetable/get',
-        headers: {
-          'X-Username-Header': localStorage.getItem('username'),
-          'X-Timestamp-Header': localStorage.getItem('timestamp'),
-          'X-Auth-Header': localStorage.getItem('auth')
-        },
-        type: 'GET'
+    events: (start, end, timezone, callback) => {
+
+      this.http.get(`https://dashboard.pclub.in/api/timetable/get?start=${start.format()}&end=${end.format()}`)
+                    .subscribe((res) => {
+                      localStorage.setItem(`${start.format()}-${end.format()}`, JSON.stringify(res.json()));
+                      callback(res.json());
+                    }, (err) => {
+                      console.log("Error encounter");
+                      if (localStorage.getItem(`${start.format()}-${end.format()}`)) {
+                        callback(JSON.parse(localStorage.getItem(`${start.format()}-${end.format()}`)));
+                      }
+                    });
       }
-    ]
   };
-  constructor(private dialog: MdDialog) { }
+
+  constructor(private dialog: MdDialog,
+              private http: HttpWrapperService) { }
+
   ngOnInit() {}
 
 }
